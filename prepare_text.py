9e58ce176de763b9abb29cfcd0235fd314112e54
Dark_Dmake <dark.dmake@gmail.com>
2019-08-16 21:33:35 +0300

    правки багов,отдельная функция для рандомных вопросов

--- prepare_text.py
+++ prepare_text.py
@@ -4,26 +4,33 @@ import random
 
 def prepare_answer(answer):
     match_obj=re.search(r"^(\w|\s)+\b",answer.replace('"',''))
-    return match_obj.group(0)
+    prepared_answer=match_obj.group(0)
+    return prepared_answer.strip()
 
-def filter(text):
-    return re.split(r":",text.replace('\n',''),1)
+def text_filter(text):
+    return re.split(r":",text.replace('\n',' '),1)
 
-def prepare_tests():
+def get_random_questions_and_answers():
+
+    return random.sample(prepare_questions_and_answers().items(), 1)
+
+
+
+def prepare_questions_and_answers(filepath='./quiz-questions/1vs1201.txt'):
 
     questions_and_answers={}
-    with open('./quiz-questions/1vs1201.txt','r', encoding="koi8-r") as data:
-        text=data.read()
+    with open(filepath,'r', encoding="koi8-r") as file:
+        text=file.read()
         
         for string in text.split('\n\n'):
             if 'Вопрос ' in string:
-                question=filter(string)[1]
+                question=text_filter(string)[1]
                 questions_and_answers[question]=None
             elif 'Ответ:\n' in string:
-                answer= filter(string)[1]
+                answer= text_filter(string)[1]
                 questions_and_answers[question]=answer
          
-    return random.sample( questions_and_answers.items(),1)
+    return questions_and_answers
 
 if __name__ == "__main__":
-    prepare_tests()
+    get_random_questions_and_answers()
